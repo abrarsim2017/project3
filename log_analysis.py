3a948d95033165476f792196a86e742f2d578a8a
abrarsim2017 <abrarsim2017@gmail.com>
2020-09-18 06:42:34 -0500

    Added calculation method
    Calculate total requests and last year requests

--- log_analysis.py
+++ log_analysis.py
@@ -1,9 +1,24 @@
 import requests
 
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-get_url = requests.get(url, allow_redirects = True)
+get_url = requests.get(url)
 
-open('logs.txt' , 'wb').write(get_url.content)
+with open('logs.txt' , 'wb') as m:
+  m.write(get_url.content)
 
-lines = 0
-requests = 0
+tr = 0
+yr = 0
+
+m = open('logs.txt', "r")
+
+lines = m.readlines()
+
+for line in lines:
+  tr = tr + 1
+  if line.find("1995") != -1:
+    yr = yr + 1
+
+m.close()
+
+print(tr)
+print(yr)
